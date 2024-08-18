3d49589a507634541880939c25f3742c8988bc63
udbhavbalaji <udbhavbalaji@gmail.com>
2024-08-18 19:29:11 -0400

    Updated path where the pickled model is stored

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,6 @@ import pickle
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'cdeac8f173801449a79cf1c89ab2b757'
 
-model = pickle.load(open('model/model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 from app import routes
